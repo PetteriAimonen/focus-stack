3768ef7b83a050983b94f66ff8dbc9474a710266
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2019-09-06 21:55:34 +0300

    Process input images in batches to reduce memory usage

--- src/focusstack.cc
+++ src/focusstack.cc
@@ -30,126 +30,113 @@ void FocusStack::run()
     if (m_reference < 0 || m_reference >= m_inputs.size())
       m_reference = m_inputs.size() / 2;
 
-    // Load input images
-    std::vector<std::shared_ptr<Task_LoadImg> > input_imgs;
+    // Load the reference image
+    // This is needed when processing the other images, so load it first.
+    std::shared_ptr<Task_LoadImg> refcolor = std::make_shared<Task_LoadImg>(m_inputs.at(m_reference));
+    std::shared_ptr<Task_Grayscale> refgray = std::make_shared<Task_Grayscale>(refcolor);
+    worker.add(refcolor);
+    worker.add(refgray);
+
+    // This loop goes through input images and processes them up to the merge step.
+    // Merging is done in batches to reduce memory usage.
+    std::vector<std::shared_ptr<ImgTask> > input_imgs;
+    std::vector<std::shared_ptr<ImgTask> > aligned_imgs;
+    std::vector<std::shared_ptr<ImgTask> > aligned_grayscales;
+    std::vector<std::shared_ptr<ImgTask> > merge_batch;
     for (int i = 0; i < m_inputs.size(); i++)
     {
-      std::shared_ptr<Task_LoadImg> task = std::make_shared<Task_LoadImg>(m_inputs.at(i));
-      input_imgs.push_back(task);
+      std::shared_ptr<Task_LoadImg> color;
+      std::shared_ptr<ImgTask> grayscale;
 
       if (i == m_reference)
       {
-        // Load reference first to have it available sooner
-        worker.prepend(task);
+        input_imgs.push_back(refcolor);
+        color = refcolor;
+        grayscale = refgray;
       }
       else
       {
-        worker.add(task);
+        color = std::make_shared<Task_LoadImg>(m_inputs.at(i));
+        worker.add(color);
+        input_imgs.push_back(color);
+
+        // Convert image to grayscale
+        // The reference image is used to calculate the best mapping, which is then used for all images.
+        grayscale = std::make_shared<Task_Grayscale>(color, refgray);
+        worker.add(grayscale);
       }
-    }
 
-    // Convert images to grayscale
-    // The reference image is used to calculate the best mapping, which is then used for all images.
-    // For that reason it is important that the tasks on reference image are prepended instead of waiting
-    // behind all other load tasks.
-    std::shared_ptr<Task_Grayscale> grayscale_reference = std::make_shared<Task_Grayscale>(input_imgs.at(m_reference));
-    worker.prepend(grayscale_reference);
-
-    std::vector<std::shared_ptr<ImgTask> > grayscale_imgs;
-    for (int i = 0; i < input_imgs.size(); i++)
-    {
-      if (i != m_reference)
+      if (m_save_steps)
       {
-        std::shared_ptr<ImgTask> task = std::make_shared<Task_Grayscale>(input_imgs.at(i), grayscale_reference);
-        worker.add(task);
-        grayscale_imgs.push_back(task);
+        worker.add(std::make_shared<Task_SaveImg>("grayscale_" + grayscale->basename(), grayscale));
       }
-      else
-      {
-        grayscale_imgs.push_back(grayscale_reference);
-      }
-    }
 
-    if (m_save_steps)
-    {
-      for (int i = 0; i < grayscale_imgs.size(); i++)
-      {
-        worker.add(std::make_shared<Task_SaveImg>("grayscale_" + grayscale_imgs.at(i)->basename(), grayscale_imgs.at(i)));
-      }
-    }
+      // Align image with respect to the reference
+      std::shared_ptr<ImgTask> aligned = std::make_shared<Task_Align>(refgray, refcolor, grayscale, color);
+      aligned_imgs.push_back(aligned);
+      worker.add(aligned);
 
-    // Align other images with respect to the reference
-    std::vector<std::shared_ptr<ImgTask> > aligned_imgs;
-    for (int i = 0; i < input_imgs.size(); i++)
-    {
-      std::shared_ptr<ImgTask> task = std::make_shared<Task_Align>(grayscale_reference, input_imgs.at(m_reference),
-                                                                   grayscale_imgs.at(i), input_imgs.at(i));
-      aligned_imgs.push_back(task);
-      worker.add(std::move(task));
-    }
-
-    // Save aligned images if requested
-    if (m_save_steps)
-    {
-      for (std::shared_ptr<ImgTask> img: aligned_imgs)
+      if (m_save_steps)
       {
         // Task_Align adds "aligned_" prefix to the filename, so just use that name for saving also.
-        worker.add(std::make_shared<Task_SaveImg>(img->filename(), img));
+        worker.add(std::make_shared<Task_SaveImg>(aligned->filename(), aligned));
       }
-    }
 
-    // Convert aligned images to grayscale again.
-    // We could also transform the grayscale images directly, but a new grayscale conversion is faster
-    // and results in less difference between the color and grayscale versions.
-    std::vector<std::shared_ptr<ImgTask> > aligned_grayscale;
-    for (int i = 0; i < aligned_imgs.size(); i++)
-    {
-      std::shared_ptr<ImgTask> task = std::make_shared<Task_Grayscale>(aligned_imgs.at(i), grayscale_reference);
-      aligned_grayscale.push_back(task);
-      worker.add(task);
-    }
+      // Convert aligned image to grayscale again.
+      // We could also transform the grayscale images directly, but a new grayscale conversion is faster
+      // and results in less difference between the color and grayscale versions.
+      std::shared_ptr<ImgTask> aligned_grayscale = std::make_shared<Task_Grayscale>(aligned, refgray);
+      worker.add(aligned_grayscale);
+      aligned_grayscales.push_back(aligned_grayscale);
 
-    // Wavelet transform all images
-    std::vector<std::shared_ptr<ImgTask> > wavelets;
-    for (int i = 0; i < aligned_imgs.size(); i++)
-    {
-      std::shared_ptr<ImgTask> task = std::make_shared<Task_Wavelet>(aligned_grayscale.at(i), false);
-      wavelets.push_back(task);
-      worker.add(std::move(task));
-    }
+      // Wavelet transform the image
+      std::shared_ptr<ImgTask> wavelet = std::make_shared<Task_Wavelet>(aligned_grayscale, false);
+      worker.add(wavelet);
+      merge_batch.push_back(wavelet);
 
-    if (m_save_steps)
-    {
-      for (int i = 0; i < aligned_imgs.size(); i++)
+      if (merge_batch.size() >= m_threads)
       {
-        worker.add(std::make_shared<Task_SaveImg>("wavelet_" + std::to_string(i) + ".png", wavelets.at(i)));
+        // Merge wavelet images accumulated so far
+        std::shared_ptr<ImgTask> merged = std::make_shared<Task_Merge>(merge_batch, m_consistency);
+        worker.add(merged);
+
+        merge_batch.clear();
+        merge_batch.push_back(merged);
       }
     }
 
-    // Merge images
-    std::shared_ptr<ImgTask> merged_wavelet = std::make_shared<Task_Merge>(wavelets, m_consistency);
-    worker.add(merged_wavelet);
+    // Merge the final batch of images
+    std::shared_ptr<ImgTask> merged_wavelet;
+    if (merge_batch.size() == 1)
+    {
+      merged_wavelet = merge_batch.front();
+    }
+    else
+    {
+      merged_wavelet = std::make_shared<Task_Merge>(merge_batch, m_consistency);
+      worker.add(merged_wavelet);
+    }
 
     // Denoise merged image
     std::shared_ptr<ImgTask> denoised = std::make_shared<Task_Denoise>(merged_wavelet, m_denoise);
     worker.add(denoised);
 
     // Inverse-transform merged image
-    std::shared_ptr<ImgTask> merged = std::make_shared<Task_Wavelet>(denoised, true);
-    worker.add(merged);
+    std::shared_ptr<ImgTask> merged_gray = std::make_shared<Task_Wavelet>(denoised, true);
+    worker.add(merged_gray);
 
     if (m_save_steps)
     {
-      worker.add(std::make_shared<Task_SaveImg>(merged->filename(), merged));
+      worker.add(std::make_shared<Task_SaveImg>(merged_gray->filename(), merged_gray));
     }
 
     // Reassign pixel values
-    std::shared_ptr<ImgTask> reassigntask = std::make_shared<Task_Reassign>(aligned_grayscale, aligned_imgs, merged);
+    std::shared_ptr<ImgTask> reassigntask = std::make_shared<Task_Reassign>(aligned_grayscales, aligned_imgs, merged_gray);
     std::shared_ptr<ImgTask> reassigned = reassigntask;
     worker.add(std::move(reassigntask));
 
     // Save result image
-    worker.add(std::make_shared<Task_SaveImg>(m_output, reassigned, input_imgs.at(m_reference)));
+    worker.add(std::make_shared<Task_SaveImg>(m_output, reassigned, refcolor));
 
   } // Close scope to avoid holding onto the shared_ptr's in local variables
 
