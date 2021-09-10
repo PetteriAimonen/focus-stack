0ee97d39b02710cf1d5406d3d5f8d121c1145e25
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2021-09-10 18:03:45 +0300

    Fix unittest build failure

--- src/task_grayscale_tests.cc
+++ src/task_grayscale_tests.cc
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "task_grayscale.hh"
+#include "logger.hh"
 
 namespace focusstack {
 
@@ -8,8 +9,9 @@ TEST(Task_Grayscale, PCA) {
   input = 0;
   input.at<cv::Vec3b>(32, 32)[1] = 1.0f;
 
+  std::shared_ptr<Logger> logger = std::make_shared<Logger>();
   Task_Grayscale task(std::make_shared<ImgTask>(input));
-  task.run();
+  task.run(logger);
 
   ASSERT_EQ(task.weights().at<float>(0), 0.0f);
   ASSERT_EQ(task.weights().at<float>(1), 1.0f);
